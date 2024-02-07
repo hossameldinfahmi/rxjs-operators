da5a29eecc0daaf0a9d408be9d5af209c862ae6e
Hossam Fahmy <hossam@gmail.com>
2024-02-07 16:28:18 +0200

    throttleTime

--- src/app/throttle-time/throttle-time.component.ts
+++ src/app/throttle-time/throttle-time.component.ts
@@ -8,11 +8,18 @@ import { asyncScheduler, fromEvent, map, throttleTime } from 'rxjs';
 })
 export class ThrottleTimeComponent {
   ngOnInit() {
+    // Configuration for throttleTime, specifying leading and trailing behavior
     const throttleTimeConfig = {
       leading: false,
       trailing: true,
     };
+
+    // Create an observable for click events on the document
     const click = fromEvent(document, 'click');
+
+    // Apply the map operator to log a message for each click event
+    // Apply the throttleTime operator to control the emission rate to once every 2000 milliseconds (2 seconds)
+    // asyncScheduler is used for asynchronous scheduling
     const result = click.pipe(
       map((event) => {
         console.log('click');
@@ -20,6 +27,8 @@ export class ThrottleTimeComponent {
       throttleTime(2000, asyncScheduler, throttleTimeConfig)
     );
 
+    // Subscribe to the resulting observable after applying throttleTime
+    // Log a message when a "double click" occurs (clicks within the specified time interval)
     result.subscribe((result) => {
       console.log('double clicked');
     });
